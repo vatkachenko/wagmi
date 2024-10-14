9b40025489031a36e7784fb91caa36fd03f21f88
Tom Meagher <tom@meagher.co>
2024-10-14 14:17:27 -0400

    test: sendTransactionAsync

--- packages/react/src/hooks/useSendTransaction.test.ts
+++ packages/react/src/hooks/useSendTransaction.test.ts
@@ -23,3 +23,19 @@ test('default', async () => {
 
   await disconnect(config, { connector })
 })
+
+test('async', async () => {
+  await connect(config, { connector })
+
+  const { result } = renderHook(() => useSendTransaction())
+
+  result.current.sendTransactionAsync({
+    to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+    value: parseEther('0.01'),
+  })
+  await waitFor(() => expect(result.current.isSuccess).toBeTruthy())
+
+  expect(result.current.data).toMatch(transactionHashRegex)
+
+  await disconnect(config, { connector })
+})
